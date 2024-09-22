2dd793ce42ab55fd5fb8ddeea55ac490ca818f2d
Sourav Chand <souravchand8116586038@gmail.com>
2024-09-22 13:40:51 +0530

    last update

--- scripts/bag.js
+++ scripts/bag.js
@@ -9,18 +9,17 @@ function onLoad() {
 }
 
 function displayBagSummary() {
-  let bagSummaryElement = document.querySelector('.bag-summary');
+  let bagSummaryElement = document.querySelector(".bag-summary");
   let totalItem = bagItemObjects.length;
   let totalMRP = 0;
   let totalDiscount = 0;
 
-  bagItemObjects.forEach(bagItem => {
+  bagItemObjects.forEach((bagItem) => {
     totalMRP += bagItem.original_price;
     totalDiscount += bagItem.original_price - bagItem.current_price;
   });
 
   let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
-  
 
   bagSummaryElement.innerHTML = `
     <div class="bag-details-container">
@@ -50,29 +49,27 @@ function displayBagSummary() {
 }
 
 function loadBagItemObjects() {
-  console.log(bagItems);
-  bagItemObjects = bagItems.map(itemId => {
+  bagItemObjects = bagItems.map((itemId) => {
     for (let i = 0; i < items.length; i++) {
       if (itemId == items[i].id) {
         return items[i];
       }
     }
   });
-  console.log(bagItemObjects);
 }
 
 function displayBagItems() {
-  let containerElement = document.querySelector('.bag-items-container');
-  let innerHTML = '';
-  bagItemObjects.forEach(bagItem => {
+  let containerElement = document.querySelector(".bag-items-container");
+  let innerHTML = "";
+  bagItemObjects.forEach((bagItem) => {
     innerHTML += generateItemHTML(bagItem);
   });
   containerElement.innerHTML = innerHTML;
 }
 
 function removeFromBag(itemId) {
-  bagItems = bagItems.filter(bagItemId => bagItemId != itemId);
-  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  bagItems = bagItems.filter((bagItemId) => bagItemId != itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
   loadBagItemObjects();
   displayBagIcon();
   displayBagItems();
